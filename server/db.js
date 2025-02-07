aec8ed8f73f1f35f57eae9a348c64f4fb1f8ab71
sach-in2399 <awmsachin431@gmail.com>
2025-02-07 15:36:02 +0530

    changes is db

--- server/db.js
+++ server/db.js
@@ -3,7 +3,9 @@ require('dotenv').config();
 
 const mysql = require("mysql2");
 
-const urlDB = `mysql://:${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+// const urlDB = `mysql://:${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+
+const urlDB = `mysql://root:GldNTtBVRpYiVTGOCJbhtkwLraQYKiUu@monorail.proxy.rlwy.net:56506/railway`
 
 const db = mysql.createConnection(urlDB);
 
