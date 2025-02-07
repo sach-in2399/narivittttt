1c0c7aab4d488fd4e1b62cb5228ffa4f10401be7
sach-in2399 <awmsachin431@gmail.com>
2025-02-07 15:21:28 +0530

    canges in db.js

--- server/db.js
+++ server/db.js
@@ -3,12 +3,9 @@ require('dotenv').config();
 
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,  // Use DB_HOST from .env
-  user: process.env.DB_USER,  // Use DB_USER from .env
-  password: process.env.DB_PASSWORD,  // Use DB_PASSWORD from .env
-  database: process.env.DB_DATABASE,  // Use DB_DATABASE from .env
-});
+const urlDB = `mysql://:${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+
+const db = mysql.createConnection(urlDB);
 
 db.connect((err) => {
   if (err) {
